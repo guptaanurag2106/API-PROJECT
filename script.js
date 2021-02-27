2e8a29dd302e8177f266f38f6993da4eba96dfdf
ANURAG GUPTA <75490384+guptaanurag2106@users.noreply.github.com>
2021-02-28 01:02:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function showData(){
     var para1=document.getElementById("data1");
-    var site="http://api.weatherstack.com/current?access_key=YOUR-ACCESS-KEY&query=";
+    var site="http://api.weatherstack.com/current?access_key=896830f25ded7c18de3347fecaa9e17c&query=";
     var city=document.getElementById("city").value;
     var url=site.concat(city);
     var img;
@@ -8,15 +8,15 @@ function showData(){
         document.getElementById("image").src=data.current.weather_icons
         
         console.log(data)
-        para1.innerHTML="    Temperature: "+data.current.temperature+"<br>"+"    Feels like: "+data.current.feelslike+"<br>";
+        para1.innerHTML="City: "+data.location.name+"<br>Temperature: "+data.current.temperature+"<sup>o</sup>C<br>"+"Feels like: "+data.current.feelslike+"<sup>o</sup>C<br>Humidity: "+data.current.humidity;
     }
-    fetch(url).then(response => response.json()).then(data =>def(data));
+    fetch(url).then(response => response.json()).then(data =>def(data)).catch(res => alert("CITY NOT FOUND!! PLEASE ENTER A VALID CITY"));
 
 }
 
 function showNews(){
     var para2=document.getElementById("data2");
-    var url2 = "http://api.mediastack.com/v1/news?access_key=YOUR-ACCESS-KEY&sources=cnn,bbc";
+    var url2 = "http://api.mediastack.com/v1/news?access_key=cf71d5bda9c860845f8ad7488fef0abc&sources=cnn,bbc";
     
     function def1(data1){
         console.log(data1)
